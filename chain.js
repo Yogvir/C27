3552e86131ff365796541cd489a35f1b1f7efecb
Yogvir <81627064+Yogvir@users.noreply.github.com>
2021-05-07 19:55:12 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain{
+constructor(bodyA,bodyB){
+
+    var options = {
+bodyA : bodyA,
+bodyB: bodyB,
+stiffness: 0.7,
+length:5,
+    }
+
+   this.body =  Constraint.create(options);
+    World.add(world,this.body);
+}
+display(){
+    var pointA = this.body.bodyA.position;
+    var pointB = this.body.bodyB.position;
+    strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+}
+
